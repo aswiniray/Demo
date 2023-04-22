56644192e4277b5c43746cd0e95ef1daa68be243
Aswini Ray <aswiniray@gmail.com>
2023-04-23 02:07:38 +1000

    Update Jwt.go

--- Jwt.go
+++ Jwt.go
@@ -1,46 +1,28 @@
 package main
 
 import (
-    "context"
     "fmt"
-    "github.com/google/go-github/v52/github"
+    "github.com/google/go-github/v35/github"
     "os"
 )
 
 func main() {
-    // Get the app ID, installation ID, and private key from environment variables.
-    appID := os.Getenv("GITHUB_APP_ID")
-    installationID := os.Getenv("GITHUB_INSTALLATION_ID")
-    privatePEMKeyPath := os.Getenv("GITHUB_PRIVATE_KEY_PEM_PATH")
-
-    // Read the private key from the file.
-    privatePEMKey, err := os.ReadFile(privatePEMKeyPath)
-    if err != nil {
-        panic(err)
-    }
+    // Get the app id, installation id and private key from environment variables.
+    appId := os.Getenv("GITHUB_APP_ID")
+    installationId := os.Getenv("GITHUB_INSTALLATION_ID")
+    privateKey := os.Getenv("GITHUB_PRIVATE_KEY")
 
     // Create a new GitHub client.
     client := github.NewClient(nil)
 
-    // Create a new JWT transport using the app ID, installation ID, and private key.
-    transport, err := github.NewAppsTransportKeyFromPEM(client.Transport, appID, installationID, privatePEMKey)
+    // Create a new app installation token.
+    token, err := client.Apps.CreateAppInstallationToken(appId, installationId, &github.CreateAppInstallationTokenOptions{
+        PrivateKey: privateKey,
+    })
     if err != nil {
         panic(err)
     }
 
-    // Set the transport on the client.
-    client.Transport = transport
-
-    // Create a new context.
-    ctx := context.Background()
-
-    // Generate a new GitHub access token.
-    accessToken, err := client.Apps.CreateAccessToken(ctx, appID, installationID)
-    if err != nil {
-        panic(err)
-    }
-
-    // Print the access token.
-    fmt.Println(accessToken.Token)
+    // Print the token.
+    fmt.Println(token.Token)
 }
-
